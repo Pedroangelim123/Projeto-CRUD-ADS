c472fc3f88edbd89e0fc71ddccb509a23d2c9e8e
Pedro Angelim <Pedroangelim10@gmail.com>
2019-11-07 21:01:13 -0200

    Salvar Usuário

---
+++ ProjetoCRUD/src/univs/edu/usuario/Main.java
@@ -0,0 +1,20 @@
+
+package univs.edu.usuario;
+
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Usuario usuario = new Usuario();
+        UsuarioDAO dao = new UsuarioDAO();
+        
+        usuario.setLogin("admin");
+        usuario.setNomeUsuario("Pedro Angelim");
+        usuario.setSenha("123");
+        
+        dao.salvar(usuario);
+        
+        
+    }
+    
+}
